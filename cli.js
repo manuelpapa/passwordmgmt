9e5cbdaadad03b050e135f554c787697bb169fbb
manuelpapa <manulito@de-vio.com>
2020-08-24 11:59:36 +0200

    Connect passwords.json to cli.js

--- cli.js
+++ cli.js
@@ -1,6 +1,25 @@
 const inquirer = require("inquirer");
 
+const fs = require("fs");
+const { async } = require("rxjs");
+function getPassword(password) {
+  try {
+    const data = fs.readFileSync("./passwords.json", "utf8");
+    const yourPassword = JSON.parse(data);
+    // console.log(yourPassword[password]);
+    //   console.log(data);
+    return yourPassword[password];
+  } catch (err) {
+    console.error(err);
+  }
+}
+
 const questions = [
+  {
+    type: "input",
+    name: "username",
+    message: "What's your username?",
+  },
   {
     type: "password",
     name: "password",
@@ -8,12 +27,16 @@ const questions = [
   },
   {
     type: "input",
-    name: "name",
-    message: "What's your name?",
+    name: "key",
+    message: "Which password do you need?",
   },
 ];
 
-inquirer.prompt(questions).then((answers) => {
-  console.log(`Your password is ${answers.password}`);
-  console.log(`Your name is ${answers.name}`);
+inquirer.prompt(questions).then(async (answers) => {
+  if (answers.password === "123" && answers.username === "papa") {
+    console.log(
+      `Hi ${answers[`username`]}, your needed password for ${answers.key} is: 
+      ${await getPassword(answers.key)}.`
+    );
+  } else console.log("Your password or username is wrong");
 });
