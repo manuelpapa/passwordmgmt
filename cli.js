3ccb75bb9e5be3d71327e7d4a0327d8efa6f076f
manuelpapa <manulito@de-vio.com>
2020-08-25 15:57:50 +0200

    Add write function

--- cli.js
+++ cli.js
@@ -1,7 +1,7 @@
 const {
   askStartQuestions,
+  askGetPasswordQuestions,
   askSetPasswordQuestions,
-  askPasswordQuestions,
   CHOICE_GET,
   CHOICE_SET,
 } = require("./lib/questions");
@@ -13,16 +13,17 @@ async function main() {
   if (masterPassword === "123") {
     console.log("Master Password is correct!");
     if (action === CHOICE_GET) {
-      console.log("Get a password");
-      const { key } = await askPasswordQuestions;
+      console.log("Now Get a password");
+      const { key } = await askGetPasswordQuestions();
       try {
         const password = await readPassword(key);
         console.log(`Your ${key} password is ${password}`);
       } catch (error) {
         console.error("Something went wrong 😑");
+        // What to do now?
       }
     } else if (action === CHOICE_SET) {
-      console.log("Now set a password");
+      console.log("Now Set a password");
       const { key, password } = await askSetPasswordQuestions();
       console.log(`New Password: ${key} = ${password}`);
     }
