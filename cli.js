4d4db6df31409118b0c0b0e4b36d4d267ae1bf15
manuelpapa <manulito@de-vio.com>
2020-08-26 14:14:42 +0200

    Hash master password and compare input hash password with hash master password

--- cli.js
+++ cli.js
@@ -2,6 +2,7 @@ const {
   askStartQuestions,
   askGetPasswordQuestions,
   askSetPasswordQuestions,
+  askForNewMasterPassword,
   CHOICE_GET,
   CHOICE_SET,
 } = require("./lib/questions");
@@ -9,33 +10,53 @@ const {
   readPassword,
   writePassword,
   readMasterPassword,
+  writeMasterPassword,
 } = require("./lib/passwords");
-const { encrypt, decrypt } = require("./lib/crypto");
+const { encrypt, decrypt, createHash, verifyHash } = require("./lib/crypto");
 
 async function main() {
+  const originalMasterPassword = await readMasterPassword();
+
+  if (!originalMasterPassword) {
+    const { newMasterPassword } = await askForNewMasterPassword();
+    const hashMasterPassword = await createHash(newMasterPassword);
+    await writeMasterPassword(hashMasterPassword);
+    console.log("Master password set!");
+    return;
+  }
+
   const { masterPassword, action } = await askStartQuestions();
-  const originalMasterPassword = await readMasterPassword;
-  if (masterPassword !== originalMasterPassword) {
+  const comparedPassword = await verifyHash(
+    masterPassword,
+    originalMasterPassword
+  );
+  if (!comparedPassword) {
     console.log("Master password is incorrect!");
-    if (action === CHOICE_GET) {
-      console.log("Now Get a password");
-      const { key } = await askGetPasswordQuestions();
-      try {
-        const encryptedPassword = await readPassword(key);
-        const password = decrypt(encryptedPassword, masterPassword);
-        console.log(`Your ${key} password is ${password}`);
-      } catch (error) {
-        console.error("Something went wrong 😑");
-        // What to do now?
-      }
-    } else if (action === CHOICE_SET) {
-      console.log("Now Set a password");
+    return;
+  }
+
+  console.log("Master password is correct!");
+  if (action === CHOICE_GET) {
+    console.log("Now Get a password");
+    const { key } = await askGetPasswordQuestions();
+    try {
+      const encryptedPassword = await readPassword(key);
+      const password = decrypt(encryptedPassword, masterPassword);
+      console.log(`Your ${key} password is ${password}`);
+    } catch (error) {
+      console.error("Something went wrong 😑");
+      // What to do now?
+    }
+  } else if (action === CHOICE_SET) {
+    console.log("Now Set a password");
+    try {
       const { key, password } = await askSetPasswordQuestions();
       const encrypedPassword = encrypt(password, masterPassword);
       await writePassword(key, encrypedPassword);
       console.log(`New password set`);
+    } catch (error) {
+      console.error("Something went wrong 😑");
     }
   }
 }
-
 main();
