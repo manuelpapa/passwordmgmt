ef3369666077a257ab764b78f28ad78191b890f7
manuelpapa <manulito@de-vio.com>
2020-08-26 11:19:29 +0200

    Outsorce decrypt/encrypt-function to cli.js

--- cli.js
+++ cli.js
@@ -6,6 +6,7 @@ const {
   CHOICE_SET,
 } = require("./lib/questions");
 const { readPassword, writePassword } = require("./lib/passwords");
+const { encrypt, decrypt } = require("./lib/crypto");
 
 async function main() {
   const { masterPassword, action } = await askStartQuestions();
@@ -16,7 +17,8 @@ async function main() {
       console.log("Now Get a password");
       const { key } = await askGetPasswordQuestions();
       try {
-        const password = await readPassword(key);
+        const encryptedPassword = await readPassword(key);
+        const password = decrypt(encryptedPassword, masterPassword);
         console.log(`Your ${key} password is ${password}`);
       } catch (error) {
         console.error("Something went wrong 😑");
@@ -25,7 +27,8 @@ async function main() {
     } else if (action === CHOICE_SET) {
       console.log("Now Set a password");
       const { key, password } = await askSetPasswordQuestions();
-      await writePassword(key, password, masterPassword);
+      const encrypedPassword = encrypt(password, masterPassword);
+      await writePassword(key, encrypedPassword);
       console.log(`New password set`);
     }
   } else {
